03be25f35444f0cacf17f3f7000b9ea6dd6ce070
aline.luiza <aline.luiza@dataprev.gov.br>
2019-07-24 14:21:04 -0300

    correcao na configuracao cors backend.integrando app com backend

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -1,6 +1,6 @@
 module.exports = function(requisicao, resposta, next){
-    requisicao.header('Access-Control-Allow-Origin','*')
-    requisicao.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, PATCH, DELETE')
-    requisicao.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
+    resposta.header('Access-Control-Allow-Origin','*')
+    resposta.header('Access-Control-Allow-Methods','GET, POST, OPTIONS, PUT, PATCH, DELETE')
+    resposta.header('Access-Control-Allow-Headers','Origin, X-Requested-With, Content-Type, Accept')
     next()
 }
