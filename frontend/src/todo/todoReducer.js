571296ef4360f69c7c455aa019109b48c5926911
aline.luiza <aline.luiza@dataprev.gov.br>
2019-07-29 14:42:06 -0300

    adicionando action para pesquisa. adicionando middleware para promise axios

--- frontend/src/todo/todoReducer.js
+++ frontend/src/todo/todoReducer.js
@@ -1,28 +1,12 @@
-const INITIAL_STATE = {
-    description: "ler livro",
-    list: 
-    [{
-        _id: 1,
-        description: "Pagar fatura cartao",
-        done:true
-    }, 
-    {
-        _id: 2,
-        description: "Reuniao diaria as 10:00",
-        done:false
-    }, 
-    {
-        _id: 3,
-        description: "Consulta medica as 17:30",
-        done:false
-    }]
-}
+const INITIAL_STATE = { description: "", list: [] }
 
 export default (state = INITIAL_STATE, action) => {
     console.log('entrou no reducer')
     switch(action.type){
         case 'DESCRIPTION_CHANGED':
             return { ...state, description: action.payload }
+        case 'TODO_SEARCHED':
+            return {...state, list: action.payload.data}
         default:
             return state
     }
